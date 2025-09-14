d50b0886d110994c81946f5ab1b5c85e7424ed34
isaistvo <9eksabit@gmail.com>
2025-09-14 21:53:44 +0300

    added lab3.s

---
+++ lab3/lab3.s
@@ -0,0 +1,18 @@
+global _start
+
+section .text
+_start:
+    ; Syscall 'write'
+    mov rax, 1          ; System call number for write
+    mov rdi, 1          ; File descriptor (1 = stdout)
+    mov rsi, message    ; Address of the message
+    mov rdx, 12         ; Length of the message
+    syscall             ; Execute the system call
+
+    ; System call 'exit'
+    mov rax, 60         ; System call number for exit
+    xor rdi, rdi        ; Exit status 0
+    syscall             ; Execute the system call
+
+section .data
+message: db "KI-241 Isai", 10
